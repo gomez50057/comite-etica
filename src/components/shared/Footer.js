5677b1d9bae34404d115c490570fd234297d6d1d
Gabriel Gomez <gomez.50057@gmail.com>
2026-02-25 13:56:01 -0600

    Se actualiza el Footer para usara moduke css

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,32 +1,44 @@
-import './Footer.css';
+import styles from "./Footer.module.css";
+
 const imgBasePath = "https://bibliotecadigitaluplaph.hidalgo.gob.mx/img_banco/footer/";
 
 const Footer = () => {
   return (
-    <footer id='footer' className="footer">
-      <div className="footer_redes">
-        <a href="https://www.facebook.com/profile.php?id=100069229599131" target="_blank" rel="noopener noreferrer">
+    <footer id="footer" className={styles.footer}>
+      <div className={styles.footerRedes}>
+        <a
+          href="https://www.facebook.com/profile.php?id=100069229599131"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={`${imgBasePath}facebook.webp`} alt="Logo de Facebook" />
         </a>
-        <a href="https://www.instagram.com/gobiernohidalgo/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.instagram.com/gobiernohidalgo/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={`${imgBasePath}instagram.webp`} alt="Logo de Instagram" />
         </a>
-        <a href="https://www.youtube.com/@GobiernoHidalgoMx" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.youtube.com/@GobiernoHidalgoMx"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={`${imgBasePath}youtube.webp`} alt="Logo de YouTube" />
         </a>
-        <a href="https://x.com/PlaneacionHgo" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://x.com/PlaneacionHgo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={`${imgBasePath}x.webp`} alt="Logo de X" />
         </a>
       </div>
 
-      {/* <div className="image-container">
-                <img src={`${imgBasePath}logo_footer.png`} alt="img_representativa" />
-            </div> */}
-
-      <div className="footer_contacto">
-
-        <div className="footer_contacto_txt">
-          <div className="footer_contacto_ico">
+      <div className={styles.footerContacto}>
+        <div className={styles.footerContactoTxt}>
+          <div className={styles.footerContactoIco}>
             <img src={`${imgBasePath}telefono.webp`} alt="icono de un telefono" />
             <div>
               <p><span>CONTACTO:</span></p>
@@ -36,10 +48,11 @@ const Footer = () => {
 
           <p><span>Lic. Arlett Correa Silva</span></p>
           <p>Secretaria Ejecutiva Propietaria</p>
-
-          {/* <p><span>Unidad de Planeación y Prospectiva</span></p> */}
+          <p><span>Unidad de Planeación y Prospectiva</span></p>
           <p>arlett.correa@hidalgo.gob.mx</p>
-          <div className="linea_footer"></div>
+
+          <div className={styles.lineaFooter}></div>
+
           <p>Gobierno del Estado de Hidalgo</p>
           <p><span>www.hidalgo.gob.mx</span></p>
         </div>
